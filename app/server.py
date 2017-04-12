cdcbf8799e0f539ff3156bc3090e2af2b84a4c25
Ankit <ankitbhagat20101994@gmail.com>
2017-04-12 10:09:18 -0400

    Removed models from server

--- app/server.py
+++ app/server.py
@@ -5,7 +5,6 @@ from redis.exceptions import ConnectionError
 from flask import Flask, Response, jsonify, request, json, url_for, make_response
 from flask_api import status    # HTTP Status Codes
 from werkzeug.exceptions import NotFound
-from models import Pet
 from . import app
 
 import error_handlers
